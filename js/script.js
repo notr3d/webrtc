5f7f2d31e19884a192a8dd60ceb8cb8ab6c7f5ee
Александр Некрасов <lxmrrr@gmail.com>
2016-04-04 17:25:45 +0300

    добавление конейнера не видео

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var webrtc = new SimpleWebRTC({
 	localVideoEl: 'localVideo',
-	remoteVideosEl: 'remotesVideos',
+	remoteVideosVideosEl: '',
 	autoRequestMedia: true
 });			
 
@@ -8,6 +8,34 @@ webrtc.on('readyToCall', function () {
 	webrtc.joinRoom('bigmsk');
 });
 
-$('.toggle-left-panel').click(function(){
-	alert(1);
+// a peer video has been added
+webrtc.on('videoAdded', function (video, peer) {
+    console.log('video added', peer);
+    var remoteVideos = document.getElementById('remoteVideos');
+    if (remoteVideos) {
+        var container = document.createElement('div');
+        var header = document.createElement('h6');//
+		var videoId = webrtc.getDomId(peer).split('_');//
+        container.className = 'videoContainer';
+        container.id = 'container_' + webrtc.getDomId(peer);
+		header.innerHTML = videoId[0];//
+        container.appendChild(video);
+		container.appendChild(header);//
+		
+        // suppress contextmenu
+        video.oncontextmenu = function () { return false; };
+
+        remoteVideos.appendChild(container);
+    }
 });
+
+// a peer video was removed
+webrtc.on('videoRemoved', function (video, peer) {
+    console.log('video removed ', peer);
+    var remotes = document.getElementById('remoteVideos');
+    var el = document.getElementById(peer ? 'container_' + webrtc.getDomId(peer) : 'localScreenContainer');
+    if (remotes && el) {
+        remotes.removeChild(el);
+    }
+});
+
