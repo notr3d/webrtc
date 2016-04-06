fe3029684ad3fd0cce91c22381633f842eeddc4b
Александр Некрасов <lxmrrr@gmail.com>
2016-04-06 12:37:28 +0300

    добавление рабочего чата

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var webrtc = new SimpleWebRTC({
 	autoRequestMedia: true
 });			
 
-webrtc.on('readyToCall', function () {
+webrtc.on('readyToCall', function () { //connectionReady here
 	webrtc.joinRoom('bigmsk');
 });
 
@@ -18,14 +18,39 @@ webrtc.on('videoAdded', function (video, peer) {
 		var videoId = webrtc.getDomId(peer).split('_');//
         container.className = 'videoContainer';
         container.id = 'container_' + webrtc.getDomId(peer);
-		header.innerHTML = videoId[0];//
+				header.innerHTML = videoId[0];//
         container.appendChild(video);
-		container.appendChild(header);//
+				container.appendChild(header);//
 		
         // suppress contextmenu
         video.oncontextmenu = function () { return false; };
 
         remoteVideos.appendChild(container);
+    }
+	  // show the ice connection state
+    if (peer && peer.pc) {
+        var connstate = document.createElement('div');
+        connstate.className = 'connectionstate';
+        container.appendChild(connstate);
+        peer.pc.on('iceConnectionStateChange', function (event) {
+            switch (peer.pc.iceConnectionState) {
+            case 'checking':
+                connstate.innerText = 'Connecting to peer...';
+                break;
+            case 'connected':
+            case 'completed': // on caller side
+                connstate.innerText = 'Connection established.';
+                break;
+            case 'disconnected':
+                connstate.innerText = 'Disconnected.';
+                break;
+            case 'failed':
+                break;
+            case 'closed':
+                connstate.innerText = 'Connection closed.';
+                break;
+            }
+        });
     }
 });
 
@@ -38,6 +63,62 @@ webrtc.on('videoRemoved', function (video, peer) {
         remotes.removeChild(el);
     }
 });
+
+/*
+// local p2p/ice failure
+webrtc.on('iceFailed', function (peer) {
+    var connstate = document.querySelector('#container_' + webrtc.getDomId(peer) + ' .connectionstate');
+    console.log('local fail', connstate);
+    if (connstate) {
+        connstate.innerText = 'Connection failed.';
+        fileinput.disabled = 'disabled';
+    }
+});
+
+// remote p2p/ice failure
+webrtc.on('connectivityError', function (peer) {
+    var connstate = document.querySelector('#container_' + webrtc.getDomId(peer) + ' .connectionstate');
+    console.log('remote fail', connstate);
+    if (connstate) {
+        connstate.innerText = 'Connection failed.';
+        fileinput.disabled = 'disabled';
+    }
+});
+*/
+
+
+//------------------text-chat-----------------
+
+ // Await messages from others
+var chatPanel = $('#chatPanel'),
+	chatText = $('#chatText'),
+	chatSend = $('#chatSend');
+
+webrtc.connection.on('message', function(data){
+	if (data.type === 'chat'){
+		console.log('message received', data);
+		chatPanel.append('<div class="message"><span class="message__name">' + data.payload.nick + ': </span><span class="message__text">' + data.payload.message + '</span></div>');
+	}
+});
+
+chatText.on('keyup', function(){
+	if (chatText.val() !== '') {	
+		chatSend.attr('disabled', false);	
+	} else {
+		chatSend.attr('disabled', true);
+	}
+});
+
+// Send a chat message
+chatSend.click(function(){
+	var message = chatText.val();
+	console.log('message sent');
+	webrtc.sendToAll('chat', {message: message, nick: webrtc.config.nick});
+	chatPanel.append('<div class="message message--yours"><span class="message__name">You: </span><span class="message__text">' + message + '</span></div>');
+	chatText.val('');
+	chatSend.attr('disabled', true);
+});
+
 
 
 
